4a32972abcd68dc2b79a7a78fd9bb59d5c7af95f
Brett Kolodny <brettkolodny@gmail.com>
2021-03-02 18:05:07 -0500

    Setup

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const whitelist = require("./whitelist");
+
+const botBanReason = "Communal Mod: User was banned from";
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("Ready to ban");
+});
+
+client.on("guildBanAdd", async (guild, user) => {
+  if (!whitelist.includes(guild.id)) {
+    console.log(`Non-whitelisted server tried to use bot: ${guild.id}`);
+    return;
+  }
+
+  const banReason = (await guild.fetchBan(user)).reason;
+  if (banReason && banReason.slice(0, botBanReason.length) == botBanReason) {
+    return;
+  }
+
+  const botGuilds = client.guilds;
+
+  botGuilds.cache.forEach((g) => {
+    if (guild.id != g.id) {
+      console.log(`Banning ${user.id} on ${g.name}`);
+
+      g.members
+        .ban(user.id, {
+          days: 1,
+          reason: `${botBanReason} ${guild.name}`,
+        })
+        .catch((error) => console.log(error));
+    }
+  });
+});
+
+client.login(process.env.TOKEN).catch(() => {
+  console.log("Error logging in");
+  process.exit(1);
+});
---
+++ src/whitelist.js
@@ -0,0 +1,7 @@
+const whitelist = [
+  "722223075629727774", // Polkadot
+  "771178421522268191", // Kusama
+  "709208197549785148", // Acala
+];
+
+module.exports = whitelist;
