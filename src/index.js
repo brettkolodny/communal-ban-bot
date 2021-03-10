c83f1b9f2150d0a58b813e4050376c8a20cac0f8
Brett Kolodny <brettkolodny@gmail.com>
2021-03-09 21:14:00 -0500

    Updated bot to allow whitelisted users to ban through command

--- src/index.js
+++ src/index.js
@@ -3,24 +3,64 @@ const whitelist = require("./whitelist");
 
 const botBanReason = "Communal Mod: User was banned from";
 
-const BREADID = "241040941073432577";
-
 const client = new Discord.Client();
+
+const admin = new Discord.User(client, { id: process.env.ADMIN });
 
 client.on("ready", () => {
   console.log("Ready to ban");
 });
 
-client.on("message", (message) => {
-  const [command, banId] = message.content.split(" ");
-  if (command == "!ban" && message.author.id == BREADID) {
-    client.guilds.cache.forEach((guild) => {
-      guild.members.ban(banId, {
-        days: 1,
-        reason: `${botBanReason} direct message by Brett`,
-      });
+client.on("message", async (message) => {
+  const senderId = message.author.id;
 
-      message.reply(`Banned from ${guild.name}`);
+  if (senderId == client.user.id) return;
+
+  const [command, banId] = message.content.split(" ");
+
+  if (command == "!ban") {
+    let isWhitelisted = false;
+
+    const botGuilds = Array.from(client.guilds.cache);
+
+    for (let [id, guild] of botGuilds) {
+      if (whitelist.includes(id)) {
+        const member = await guild.members.fetch(senderId);
+
+        if (
+          member &&
+          member.hasPermission(Discord.Permissions.FLAGS.BAN_MEMBERS)
+        ) {
+          isWhitelisted = true;
+          break;
+        }
+      }
+    }
+
+    if (!isWhitelisted) {
+      message.reply("You do not have permission to interact with this bot");
+      console.log(`User: ${senderId} attempted to use ban command`);
+      admin.dmChannel.send(`User: ${senderId} attempted to use ban command`);
+      return;
+    }
+
+    client.guilds.cache.forEach((guild) => {
+      guild.members
+        .ban(banId, {
+          days: 1,
+          reason: `${botBanReason} direct message by Brett`,
+        })
+        .catch((error) => {
+          console.log(error);
+          return true;
+        })
+        .then((error) => {
+          if (error) {
+            message.reply("There was an error");
+          } else {
+            message.reply(`Banned from ${guild.name}`);
+          }
+        });
     });
   }
 });
@@ -46,7 +86,9 @@ client.on("guildBanAdd", async (guild, user) => {
           days: 1,
           reason: `${botBanReason} ${guild.name}`,
         })
-        .catch((error) => console.log(error));
+        .catch((error) => {
+          console.log(error);
+        });
     }
   });
 });
