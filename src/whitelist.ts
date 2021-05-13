31d4edf6293ffa58d3c47275d3daa123ff3433c0
Dan Shields <35669742+NukeManDan@users.noreply.github.com>
2021-05-13 09:39:23 -0600

    Update whitelist.ts

--- src/whitelist.ts
+++ src/whitelist.ts
@@ -7,13 +7,13 @@ export const whitelist: Map<string, ServerSettings> = new Map();
 
 // Polkadot
 whitelist.set("722223075629727774", {
-  blackList: ["help", "support", "desk", "tech", "giveaway", "admin", "minaprotocol", "dev", "community", "mod", "assist", "wallet", "sync", "troubleshoot","assistance",],
+  blackList: ["team", "discord", "help", "support", "desk", "tech", "giveaway", "admin", "minaprotocol", "dev", "community", "mod", "assist", "wallet", "sync", "troubleshoot","assistance",],
   banOnLeave: true
 });
 
 // Kusama
 whitelist.set("771178421522268191", {
-  blackList: ["help", "support", "desk", "tech", "giveaway", "admin", "minaprotocol", "dev", "community", "mod", "assist", "wallet", "sync", "troubleshoot","assistance",],
+  blackList: ["team", "discord", "help", "support", "desk", "tech", "giveaway", "admin", "minaprotocol", "dev", "community", "mod", "assist", "wallet", "sync", "troubleshoot","assistance",],
   banOnLeave: true
 });
 
